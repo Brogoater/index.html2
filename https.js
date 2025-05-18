4322f7221d4e4604602e9c2ff9c0e6d5727ed5b3
Felix <mbiracasper13@gmail.com>
2025-05-18 16:44:25 +0300

    Add files via upload

---
+++ https.js
@@ -0,0 +1,6 @@
+<><form onsubmit="redirectToPage(); return false;">
+    <button type="submit">Submit & Go</button>
+</form><script>
+        function redirectToPage() {window.location.href = "https://example.com"};
+
+    </script></>
